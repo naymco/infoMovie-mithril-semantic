e501e243ffdf1b6d9e3aec9ea52539d37fa3076f
naymc0 <alex.santos@digitalvalue.es>
2020-05-29 21:51:13 +0200

    Mejoras en el pintado de items al buscar o no, salen las búsquedas y se elimina la página inicial

--- src/components/landPage.js
+++ src/components/landPage.js
@@ -19,8 +19,10 @@ const LandPage = () => {
 
   let search = () => {};
 
-  let valueSearch;
+  let valueSearch = null;
   let listado = [];
+  let movieDetail = null;
+
   return {
     oninit: async (vnode) => {
       load();
@@ -33,114 +35,138 @@ const LandPage = () => {
           { style: "margin: 20px 0;" },
           m(Input, {
             oninput: (event) => {
+              valueSearch = event.target.value;
               // console.log(event)
-              if (event.target.value.length > 0) {
+              if (valueSearch.length > 0) {
+                // console.log(valueSearch);
                 api_get(
                   `https://api.themoviedb.org/3/search/multi?api_key=e57408e326e739e92dd69004e24bd6e6&language=en-US&query=${event.target.value}&page=1&include_adult=false`
                 )
                   .then((res) => {
                     listado = res.results;
-                    console.log(listado);
+                    // console.log(listado);
                   })
                   .catch((error) => console.log(error));
               }
             },
-            onclick: () => {},
             type: "search",
             placeholder: "Search...",
             class: "field",
           })
         ),
-        m(
-          ".ui.four.doubling.cards",
-          listado.map((result) => {
-            return m(".card", [
-              result.poster_path
-                ? m(
+        listado.length > 0
+          ? m(
+              ".ui.four.doubling.cards",
+              listado.map((result) => {
+                return m(".card", [
+                  result.poster_path
+                    ? m(
+                        ".image",
+                        m("img", {
+                          src: `https://image.tmdb.org/t/p/w200/${result.poster_path}`,
+                        })
+                      )
+                    : m(
+                        ".image",
+                        m("img", {
+                          src:
+                            "https://vignette.wikia.nocookie.net/diamondnoace/images/6/6d/No_image.png/revision/latest/scale-to-width-down/340?cb=20160808012100",
+                        })
+                      ),
+
+                  m(".content", [
+                    result.title
+                      ? m(".header", result.title)
+                      : m(".header", result.name || "No results found"),
+                    result.overview
+                      ? m(".description", result.overview)
+                      : m(
+                          ".description",
+                          "We have not found any results on this film."
+                        ),
+                  ]),
+
+                  m(".ui.bottom.green.attached.button", "More info"),
+                ]);
+              })
+            )
+          : m(
+              ".ui.four.doubling.cards",
+              movies.map((movie) => {
+                // console.log(movie);
+                // console.log(movies);
+                return m(".card", [
+                  m(
                     ".image",
                     m("img", {
-                      src: `https://image.tmdb.org/t/p/w200/${result.poster_path}`,
-                    })
-                  )
-                : m(
-                    ".image",
-                    m("img", {
-                      src:
-                        "https://vignette.wikia.nocookie.net/diamondnoace/images/6/6d/No_image.png/revision/latest/scale-to-width-down/340?cb=20160808012100",
+                      src: `https://image.tmdb.org/t/p/w200/${movie.poster_path}`,
                     })
                   ),
-
-              m(".content", [
-                result.title
-                  ? m(".header", result.title)
-                  : m(".header", result.name || "No results found"),
-                result.overview
-                  ? m(".description", result.overview)
-                  : m(
-                      ".description",
-                      "We have not found any results on this film."
-                    ),
-              ]),
-
-              m(".ui.bottom.green.attached.button", "More info"),
-            ]);
-          })
-        ),
-        m(
-          ".ui.four.doubling.cards",
-          movies.map((movie) => {
-            return m(".card", [
-              m(
-                ".image",
-                m("img", {
-                  src: `https://image.tmdb.org/t/p/w200/${movie.poster_path}`,
-                })
-              ),
-              m(".content", [
-                m(".header", movie.title),
-                m(".description", movie.overview),
-              ]),
-              m(".ui.bottom.green.attached.button", "More info"),
-            ]);
-          })
-        ),
+                  m(".content", [
+                    m(".header", movie.title),
+                    m(".description", movie.overview),
+                  ]),
+                  m(
+                    ".ui.bottom.green.attached.button",
+                    {
+                      onclick: (event) => {
+                        api_get(
+                          `https://api.themoviedb.org/3/movie/${movie.id}?api_key=e57408e326e739e92dd69004e24bd6e6&language=en-US`
+                        )
+                          .then((detail) => {
+                            movieDetail = detail;
+                            console.log(movieDetail);
+                          })
+                          .catch((error) => console.log(error));
+                      },
+                    },
+                    "More info"
+                  ),
+                ]);
+              })
+            ),
         // Paginación a partir de aquí.
-        m(".ui.label", `Páginas totales: ${data.total_pages}`),
-        m(
-          ".ui.label",
-          {
-            style: "margin: 20px;",
-          },
-          `Página: ${page}`
-        ),
-        page > 1
+        listado.length < 1
           ? m(
-              "a.ui.red.button",
-              {
-                href: `./index.html#!/${page - 1}`,
-                onclick: () => {
-                  page--;
-                  load();
-                  window.scrollTo({ top: 0, behavior: "smooth" }); // efecto muy interesante
+              ".ui.container",
+              m(".ui.label", `Páginas totales: ${data.total_pages}`),
+              m(
+                ".ui.label",
+                {
+                  style: "margin: 20px;",
                 },
-              },
-              m("i.icon.arrow.left"),
-              ` ${page - 1} :Anterior`
-            )
-          : null,
-        data.total_pages > 20 * page
-          ? m(
-              "a.ui.green.button",
-              {
-                href: `./index.html#!/${page + 1}`,
-                onclick: () => {
-                  page++;
-                  load();
-                  window.scrollTo({ top: 0, behavior: "smooth" });
-                },
-              },
-              m(`i.icon.arrow.right`),
-              ` Siguiente: ${page + 1}`
+                `Página: ${page}`
+              ),
+              page > 1
+                ? m(
+                    "a.ui.red.button",
+                    {
+                      href: `./index.html#!/${page - 1}`,
+                      onclick: () => {
+                        page--;
+                        load();
+                        window.scrollTo({ top: 0, behavior: "smooth" }); // efecto muy interesante
+                      },
+                    },
+                    m("i.icon.arrow.left"),
+                    ` ${page - 1} :Anterior`
+                  )
+                : null,
+              data.total_pages > 20 * page
+                ? m(
+                    "a.ui.green.button",
+                    {
+                      href: `./index.html#!/${page + 1}`,
+                      onclick: () => {
+                        page++;
+                        load();
+                        window.scrollTo({ top: 0, behavior: "smooth" });
+                      },
+                    },
+                    m(`i.icon.arrow.right`),
+                    ` Siguiente: ${page + 1}`
+                  )
+                : null
             )
           : null,
       ]);
